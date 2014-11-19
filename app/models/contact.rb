63242cadcabef5bae24b68c0a9fcba622cd0c3ce
Laura Laugwitz <s0544049@htw-berlin.de>
2014-11-19 20:50:27 +0100

    Validate proper Email adress

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,5 +1,5 @@
 class Contact < ActiveRecord::Base
     validates :first_name, presence: true
     validates :last_name, presence: true
-    validates :email, presence: true
+    validates :email, presence: true, format: {  with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, message: "Bitte gib eine gültige Email-Adressse an"  }
 end
