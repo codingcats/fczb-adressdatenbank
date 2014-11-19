949bac9d478dc45a0be18366848390f43a37280a
Tina Schönborn <tina.schoenborn@student.htw-berlin.de>
2014-11-19 21:50:21 +0100

    Remove email column from contact

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141119203153) do
+ActiveRecord::Schema.define(version: 20141119204309) do
 
   create_table "contacts", force: true do |t|
     t.string   "first_name"
     t.string   "last_name"
-    t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
