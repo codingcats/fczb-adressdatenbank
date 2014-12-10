a16c4893eb60b98fbea8705df558946ee5ad9a14
Laura Laugwitz <s0544049@htw-berlin.de>
2014-12-10 21:58:04 +0100

    Give Name and Id Tag to additional Email fields

--- app/assets/javascripts/contacts.js
+++ app/assets/javascripts/contacts.js
@@ -3,10 +3,34 @@
 // You can use CoffeeScript in this file: http://coffeescript.org/
 
  
+
 $( document ).ready(function() {
-	$(".add-email").click(function(){
-		var input = $("<input><br>");
-		$(".email-container").append(input);
-			
-	});
+
+  function generateIndexNumber() {
+
+    //Get all input tags with type 'text'
+    var textFields = $("input[type='text']");
+
+    //Filter text fields by id attribute and return only textfields starting with contact_emails_attributes_
+    var emailTextFields = $.grep(textFields, function(textField) {
+      var id = $(textField).attr("id");
+      return id.startsWith("contact_emails_attributes_");
+    });
+    return emailTextFields.length;
+  }
+
+  function createName() {
+    return "contact[emails_attributes]["+ generateIndexNumber() +"][value]";
+  }
+
+  function createId() {
+    return "contact_emails_attributes_"+ generateIndexNumber() +"_value";
+  }
+
+  $(".add-email").click(function(){
+    var div = $("<div>").html($("<input>").attr("type","text")
+                                .attr("id", createId())
+                                .attr("name", createName()));
+    $(".email-container").append(div);
+  });
 });
