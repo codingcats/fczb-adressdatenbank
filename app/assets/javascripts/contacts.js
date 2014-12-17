abd97b5f7af7bec00d3af1116f441666927bd852
Laura Laugwitz <s0544049@htw-berlin.de>
2014-12-17 20:39:51 +0100

    Create Remove 'Button' for Emails and make its parent container destroy itself on click

--- app/assets/javascripts/contacts.js
+++ app/assets/javascripts/contacts.js
@@ -26,11 +26,19 @@ $( document ).ready(function() {
   function createId() {
     return "contact_emails_attributes_"+ generateIndexNumber() +"_value";
   }
-
+  
+  //Hier geht die Funktion los! Die CSS-Klasse .add-email (der +Button) wird geklickt und dann wird ein neues Textfeld in den Container hinzugefügt.
   $(".add-email").click(function(){
     var div = $("<div>").html($("<input>").attr("type","text")
                                 .attr("id", createId())
                                 .attr("name", createName()));
     $(".email-container").append(div);
+  });
+  
+  // Lösche die Email, wenn das Element mit der .remove-email Klasse angeklickt wird.
+  $(".remove-email").click(function(){
+    if (confirm('Die Email wird bei "OK" gelöscht.')) {
+      $( this ).parent().remove();
+    }
   });
 });
