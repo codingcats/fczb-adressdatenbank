a1f6395cdecc855facf76b25cff84d6045469b76
Laura Laugwitz <s0544049@htw-berlin.de>
2014-12-17 19:47:03 +0100

    Save previously entered emails only that one time

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -70,6 +70,6 @@ class ContactsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def contact_params
-      params.require(:contact).permit(:first_name, :last_name, emails_attributes: [:value])
+      params.require(:contact).permit(:first_name, :last_name, emails_attributes: [:value, :id])
     end
 end
