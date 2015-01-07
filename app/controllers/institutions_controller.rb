81c3767e56a9ee8701492f7e57b872fce0788e0e
Tina Schönborn <tina.schoenborn@student.htw-berlin.de>
2015-01-07 21:43:48 +0100

    Add form, new, edit and controller actions for institutions

--- app/controllers/institutions_controller.rb
+++ app/controllers/institutions_controller.rb
@@ -1,4 +1,24 @@
 class InstitutionsController < ApplicationController
   def new
+  	@institution = Institution.new
   end
+
+  def edit
+  end
+
+  def create
+  	@institution = Institution.new(institution_params)
+  	
+  	if @institution.save
+  		redirect_to @institution, notice: 'Institution wurde angelegt.'
+  	else
+  		render :new
+  	end	
+  end
+
+  private
+  	# Never trust parameters from the scary internet, only allow the white list through.
+    def institution_params
+      params.require(:institution).permit(:name, :street, :postcode, :city)
+    end
 end
