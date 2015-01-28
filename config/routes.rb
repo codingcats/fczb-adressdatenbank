870fd8d96de112b6a54ed79237787ecc48576c7b
Laura Laugwitz <s0544049@htw-berlin.de>
2015-01-28 19:51:28 +0100

    Get in routes for Handbook

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,21 @@
 Rails.application.routes.draw do
 
-  
 
+
+  get 'static_pages/home'
   get 'static_pages/home'
 
   resources :contacts
   resources :institutions
   resources :users
   resources :occasions
-  resources :handbook
 
   root 'static_pages#home'
 
   get 'sessions/new'
-  
+
   get    'login'   => 'sessions#new'
   post   'login'   => 'sessions#create'
   delete 'logout'  => 'sessions#destroy'
- 
+
 end
