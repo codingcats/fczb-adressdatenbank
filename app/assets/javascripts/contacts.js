1a62eeac93858a8979fc8878089a78636672f983
noobiba <noobanine@gmail.com>
2015-02-07 17:39:43 +0100

    Add new email row and delete button inside table.

--- app/assets/javascripts/contacts.js
+++ app/assets/javascripts/contacts.js
@@ -20,19 +20,47 @@ $( document ).ready(function() {
   }
 
   function createName() {
-    return "contact[emails_attributes]["+ generateIndexNumber() +"][value]";
+    if (arguments.length === 0){
+      var attribute = "value";
+    }
+    else {
+      var attribute = arguments[0];
+    }  
+    return "contact[emails_attributes]["+ generateIndexNumber() +"][" + attribute + "]";
   }
 
   function createId() {
-    return "contact_emails_attributes_"+ generateIndexNumber() +"_value";
+    if (arguments.length === 0){
+      var attribute = "_value";
+    }
+    else {
+      var attribute = arguments[0];
+    }  
+    return "contact_emails_attributes_"+ generateIndexNumber() + attribute;
   }
   
   //Hier geht die Funktion los! Die CSS-Klasse .add-email (der +Button) wird geklickt und dann wird ein neues Textfeld in den Container hinzugefügt.
   $(".add-email").click(function(){
-    var div = $("<div>").html($("<input>").attr("type","text")
+    /*var div = $("<div>").html($("<input>").attr("type","text")
                                 .attr("id", createId())
                                 .attr("name", createName()));
-    $(".email-container").append(div);
+    $(".email-container").append(div);*/
+
+    var row = $("<tr>");
+    var td = $("<td>");
+    var input = $("<input>").attr("type","text")
+                                .attr("id", createId())
+                                .attr("name", createName());
+    var del_but = $("<input>").attr("type","checkbox")
+                              .attr("id", createId("_destroy"))
+                              .attr("name", createName("_destroy"));
+    var delete_td = $("<td>").attr("class", "email_delete");
+
+    delete_td.append(del_but);
+    td.append(input);
+    row.append(td);
+    row.append(delete_td);
+    $(".email-container table").append(row);
   });
   
   // Löschen über Checkbox bei Submit (mit Sicherheitsabfragefenster)
