4933d01b808302a9179002b0fea1eba67c8b9e9b
Tina Schönborn <tina.schoenborn@student.htw-berlin.de>
2015-02-09 22:06:48 +0100

    Fix sorting in institution name and postcode row

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -4,10 +4,10 @@ class Contact < ActiveRecord::Base
 
     has_many :emails
     belongs_to :institution
-    scope :order_institution, ->(direction) { joins("LEFT JOIN 'institutions' ON contacts.institution_id = institutions.id").order("LOWER(institutions.name) #{direction}") }
-    scope :order_postcode, ->(direction) { joins("LEFT JOIN 'institutions' ON contacts.institution_id = institutions.id").order("LOWER(institutions.postcode) #{direction}") }
+    scope :order_institution, ->(direction) { joins("LEFT JOIN institutions ON contacts.institution_id = institutions.id").order("LOWER(institutions.name) #{direction}") }
+    scope :order_postcode, ->(direction) { joins("LEFT JOIN institutions ON contacts.institution_id = institutions.id").order("LOWER(institutions.postcode) #{direction}") }
     has_and_belongs_to_many :occasions
-    scope :filter_occasions, ->(occasion_ids) { joins("LEFT JOIN 'contacts_occasions' on contacts.id = contacts_occasions.contact_id").where('occasion_id IN (?)', occasion_ids).uniq }
+    scope :filter_occasions, ->(occasion_ids) { joins("LEFT JOIN contacts_occasions on contacts.id = contacts_occasions.contact_id").where('occasion_id IN (?)', occasion_ids).uniq }
     accepts_nested_attributes_for :emails, :allow_destroy => true
 
     acts_as_paranoid
