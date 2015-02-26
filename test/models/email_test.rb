25acbe68c88ac8a20abec28ae9cfc66ca652e259
Tina Schönborn <tina.schoenborn@student.htw-berlin.de>
2015-02-26 23:13:41 +0100

    Add email model validation tests and remove fixtures

--- test/models/email_test.rb
+++ test/models/email_test.rb
@@ -4,4 +4,14 @@ class EmailTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "email heinz@buschkows.ky is valid" do
+  	email = Email.new(value: "heinz@buschkows.ky")
+  	assert email.valid?
+  end
+
+  test "email huhu@huhu is invalid" do
+  	email = Email.new(value: "huhu@huhu")
+  	assert_not email.valid?
+  end
 end
